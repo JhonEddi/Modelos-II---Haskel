6e8c77d1aece10ac4c167ed7c34a9c470680b47b
ojhernandezcepeda <36193487+ojhernandezcepeda@users.noreply.github.com>
2018-03-13 13:55:08 -0500

    Update FuncionesListas.hs

--- FuncionesListas.hs
+++ FuncionesListas.hs
@@ -9,24 +9,6 @@ invertirLista ::[Int]->[Int]
 invertirLista [] = []
 invertirLista (x:xs) = (invertirLista xs) ++ [x]
 
---contar numeros impares de una lista recursivamente
-contarImpares::[Int]->Int
-contarImpares[] = 0
-contarImpares(x:xs)
- | mod x 2 == 1 = 1 + contarImpares(xs)
- | otherwise = contarImpares(xs)
-
---contar numeros impares con funciones de Haskell
-contarImparFunciones :: [Int]->Int
-contarImparFunciones(x:xs) = length (filter odd (x:xs))
-
---encontrar mayor de una lista recursivamente
-mayorElemento :: [Int] -> Int
-mayorElemento [a] = a
-mayorElemento (x:xs) 
- | x > mayorElemento xs = x 
- | otherwise = mayorElemento xs
-
 --contar numeros pares de una lista recursivamente
 contarPares::[Int]->Int
 contarPares[] = 0
@@ -38,8 +20,18 @@ contarPares(x:xs)
 contarParFunciones :: [Int]->Int
 contarParFunciones(x:xs) = length (filter even (x:xs))
 
---Lista dentro de una lista recursivamente
+--contar numeros impares de una lista recursivamente
+contarImpares::[Int]->Int
+contarImpares[] = 0
+contarImpares(x:xs)
+ | mod x 2 == 1 = 1 + contarImpares(xs)
+ | otherwise = contarImpares(xs)
 
+--contar numeros impares con funciones de Haskell
+contarImparFunciones :: [Int]->Int
+contarImparFunciones(x:xs) = length (filter odd (x:xs))
+
+--Lista dentro de una lista recursivamente
 --Elemento dentro de una lista
 pertenece :: Int->[Int]-> Bool
 pertenece x [] = False
@@ -53,4 +45,13 @@ perteneceLista [] lista = True
 perteneceLista lista [] = False
 perteneceLista (x:xs) (y:ys) = pertenece x (y:ys) && perteneceLista xs (y:ys)
 
---Lista dentro de una lista funciones de orden superior
+--lista dentro de una lista funciones de orden superior
+contLista::[Int]->[Int]->Bool
+contLista (x:xs) (y:yi) = and [elem t (y:yi)|t <- (x:xs)
+
+--encontrar mayor de una lista recursivamente
+mayorElemento :: [Int] -> Int
+mayorElemento [a] = a
+mayorElemento (x:xs) 
+ | x > mayorElemento xs = x 
+ | otherwise = mayorElemento xs
